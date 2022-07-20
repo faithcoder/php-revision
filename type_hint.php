2a68e58b788bcc161d0e1324b399f3497f301a1a
faithcoder <48954386+faithcoder@users.noreply.github.com>
2022-07-20 23:07:24 +0600

    type hint complete

--- type_hint.php
+++ type_hint.php
@@ -7,19 +7,39 @@
 
 
 //ARGUMENT TYPE HINTING
-function myFunc(array $num){     //এখানে B একটা object হিসেবে কাজ করছে
+// function myFunc(array $num){     //এখানে B একটা object হিসেবে কাজ করছে
+//     //var_dump($num);
+//     return $num;
+// }
+// class A {
+//     public $y=50;
+// }
+// class B {
+//     public $x = 30; 
+// }
+
+// try{
+//     var_dump(myFunc([5, 6, 7.7, "arif"]));
+// }catch(TypeError $error){   //এখানে Typeerror php এর একটা ডেফল্ট ফাংশন
+//     echo "Error shows".$error->getMessage();
+// }
+
+//RETURN TYPE HINTING
+function myFunc(A $num):int{     //এখানে A একটা object হিসেবে কাজ করছে
     //var_dump($num);
-    return $num;
+    //return "6.5";\
+    return (int)$num->num2;
 }
 class A {
     public $y=50;
+    public $num2 = "12"; //as a string 
 }
 class B {
     public $x = 30; 
 }
 
 try{
-    var_dump(myFunc([5, 6, 7.7, "arif"]));
+    var_dump(myFunc(new A));
 }catch(TypeError $error){   //এখানে Typeerror php এর একটা ডেফল্ট ফাংশন
     echo "Error shows".$error->getMessage();
 }
