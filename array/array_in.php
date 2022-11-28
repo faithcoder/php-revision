3dad6142cfcf7f0988717595224f8f03a10e8c76
faithcoder <48954386+faithcoder@users.noreply.github.com>
2022-11-28 20:23:18 +0600

    in_array_updated

--- array/array_in.php
+++ array/array_in.php
@@ -8,24 +8,24 @@ $car = [
     "Tata" => "India",
 ];
 
-// $car = array("BMW", "Toyota", "55", "Honda", "Hyundai");
+//$car = array("BMW", "Toyota", "55", "Honda", "Hyundai");
 
-// if(in_array(55, $car, true)){       // in_array function always return 1 or o, 1 is true and 0 is false
-//     echo "Found successfully";
-// }else {
-//     echo "can't find";
-// }
+if(in_array(55, $car, true)){       // in_array function always return 1 or o, 1 is true and 0 is false
+    echo "Found successfully";
+}else {
+    echo "can't find";
+}
 
 echo array_search("Japan", $car);
 
-// $data = array(
-//     array('a','b'),
-//     array('p','r'),
-//     'o'
-// );
+$data = array(
+    array('a','b'),
+    array('p','r'),
+    'o'
+);
 
-// if(in_array(array('a','b'), $data, true)){       // in_array function always return 1 or o, 1 is true and 0 is false
-//     echo "Found successfully";
-// }else {
-//     echo "can't find";
-// }
+if(in_array(array('a','b'), $data, true)){       // in_array function always return 1 or o, 1 is true and 0 is false
+    echo "Found successfully";
+}else {
+    echo "can't find";
+}
